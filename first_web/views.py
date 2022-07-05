099b15c28d493ffb4468590d2d5b2c3c080fe5d9
shengdao <211471317@qq.com>
2022-07-05 09:48:28 +0800

    shengdao'test

--- first_web/views.py
+++ first_web/views.py
@@ -1,5 +1,7 @@
 # python manage.py runserver
 # test by shengdao
+# test by shengdao2
+
 from django.shortcuts import render
 from django.conf import settings
 from django.http import HttpResponse, FileResponse
